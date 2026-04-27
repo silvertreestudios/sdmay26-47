3a0c8d77a5af77c959d8bd4d174922eed5ea72d8
TJ Pexa <pexatj@iastate.edu>
2026-04-27 04:31:20 -0500

    added in the starting screen

--- Assets/_Project/Scripts/MainMenuController.cs
+++ Assets/_Project/Scripts/MainMenuController.cs
@@ -3,36 +3,27 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
-    // Name of your game scene (must match exactly in Build Settings)
     [SerializeField] private string gameSceneName = "Level 1";
+    [SerializeField] private GameObject ORCScrollView; // drag PDFScrollView here
 
-    // Filename of your PDF inside StreamingAssets/
-    [SerializeField] private string pdfFileName = "ORC_LicenseFINAL.pdf";
+    void Start()
+    {
+        // Make sure PDF viewer is hidden on startup
+        ORCScrollView.SetActive(false);
+    }
 
-    // Called by Start Game button
     public void OnStartGame()
     {
         SceneManager.LoadScene(gameSceneName);
     }
 
-    // Called by View PDF button
     public void OnViewPDF()
     {
-        string pdfPath = "";
-
-#if UNITY_EDITOR
-        pdfPath = System.IO.Path.Combine(Application.streamingAssetsPath, pdfFileName);
-#elif UNITY_STANDALONE_WIN
-        pdfPath = System.IO.Path.Combine(Application.streamingAssetsPath, pdfFileName);
-#elif UNITY_STANDALONE_OSX
-        pdfPath = System.IO.Path.Combine(Application.streamingAssetsPath, pdfFileName);
-#endif
-
-        // Opens the PDF in the system's default PDF viewer
-        Application.OpenURL("file:///" + pdfPath);
+        // Toggle the PDF viewer on/off
+        bool isActive = ORCScrollView.activeSelf;
+        ORCScrollView.SetActive(!isActive);
     }
 
-    // Called by Quit button
     public void OnQuitGame()
     {
 #if UNITY_EDITOR
